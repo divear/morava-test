5e5e066d68f5ba33696d79297c2b700f1806d582
lukascobit <lukyn.odehnal333@gmail.com>
2022-08-12 23:24:41 +0200

    questions input

--- pages/nove-otazky.tsx
+++ pages/nove-otazky.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+import Meta from "../public/Meta";
 
 function Otazky() {
 	return (
-		<div className="center blackbox">
+		<div className="center blackbox questions">
+			<Meta title="Nová otázka" />
 			<h1>Přidej novou otázku do kvízu!</h1>
+			<label htmlFor="otazka">Otázka: </label>
+			<input type="text" id="otazka" placeholder="Odkuď jsi?" />
+			<div className="answersInput">
+				<label htmlFor="">Odpověď pro Prahu: </label>{" "}
+				<input type="text" placeholder="Praha" />
+				<label htmlFor="">Odpověď pro Brno: </label>{" "}
+				<input type="text" placeholder="Brno" />
+				<label htmlFor="">Odpověď pro Ostravu: </label>{" "}
+				<input type="text" placeholder="Ostrava" />
+				<label htmlFor="">Odpověď pro Ústí: </label>{" "}
+				<input type="text" placeholder="Ústí nad Labem" />
+			</div>
+			<button>Odeslat</button>
 		</div>
 	);
 }
