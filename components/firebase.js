894667358d8dd61e3f8dad2487462ecd310b7cf1
lukascobit <lukyn.odehnal333@gmail.com>
2022-08-14 21:31:13 +0200

    firebase boilerplate & form handeling

---
+++ components/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+	apiKey: process.env.NEXT_PUBLIC_API_KEY,
+
+	authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+
+	projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+
+	storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+
+	messagingSenderId: process.env.NEXT_PUBLIC_SENDERID,
+
+	appId: process.env.NEXT_PUBLIC_APPID,
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+export { app, getFirestore, collection, addDoc };
--- pages/nove-otazky.tsx
+++ pages/nove-otazky.tsx
@@ -1,24 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import Meta from "../components/Meta";
+import { app, getFirestore, addDoc, collection } from "../components/firebase";
+
+const db = getFirestore(app);
+
+async function addToDb() {
+	try {
+		const docRef = await addDoc(collection(db, "users"), {
+			first: "Ada",
+			last: "Lovelace",
+			born: 1815,
+		});
+		console.log("Document written with ID: ", docRef.id);
+	} catch (e) {
+		console.error("Error adding document: ", e);
+	}
+}
 
 function Otazky() {
+	const [question, setQuestion] = useState("Who?");
+	const [answer, setAnswer] = useState(["", "", "", ""]);
+	console.log(answer);
+
+	const handleChange = (index: number, value: string) => {
+		const data = [...answer];
+		data.splice(index, 1, value);
+		setAnswer(data);
+	};
 	return (
 		<div className="center blackbox questions">
 			<Meta title="Nová otázka" />
+
 			<h1>Přidej novou otázku do kvízu!</h1>
+
 			<label htmlFor="otazka">Otázka: </label>
-			<input type="text" id="otazka" placeholder="Odkuď jsi?" />
+			<input
+				value={question}
+				onChange={(e) => setQuestion(e.target.value)}
+				type="text"
+				id="otazka"
+				placeholder="Odkuď jsi?"
+			/>
+
 			<div className="answersInput">
 				<label htmlFor="">Odpověď pro Prahu: </label>{" "}
-				<input type="text" placeholder="Praha" />
+				<input
+					value={answer[0]}
+					onChange={(e) => handleChange(0, e.target.value)}
+					type="text"
+					placeholder="Praha"
+				/>
 				<label htmlFor="">Odpověď pro Brno: </label>{" "}
-				<input type="text" placeholder="Brno" />
+				<input
+					value={answer[1]}
+					onChange={(e) => handleChange(1, e.target.value)}
+					type="text"
+					placeholder="Brno"
+				/>
 				<label htmlFor="">Odpověď pro Ostravu: </label>{" "}
-				<input type="text" placeholder="Ostrava" />
+				<input
+					value={answer[2]}
+					onChange={(e) => handleChange(2, e.target.value)}
+					type="text"
+					placeholder="Ostrava"
+				/>
 				<label htmlFor="">Odpověď pro Ústí: </label>{" "}
-				<input type="text" placeholder="Ústí nad Labem" />
+				<input
+					value={answer[3]}
+					onChange={(e) => handleChange(3, e.target.value)}
+					type="text"
+					placeholder="Ústí nad Labem"
+				/>
 			</div>
-			<button>Odeslat</button>
+			<button onClick={() => addToDb}>Odeslat</button>
 		</div>
 	);
 }
